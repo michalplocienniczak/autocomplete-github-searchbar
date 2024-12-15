82ffe52842038330bd1bf03ed4239f9c884109de
mpcodingpl <97036774+mpcodingpl@users.noreply.github.com>
2024-12-15 18:53:13 +0100

    test: adds tests for a Loading Skeleton component

--- src/features/github-autocomplete/components/search-dialog/components/search-results/components/loading-skeleton/loading-skeleton.component.tsx
+++ src/features/github-autocomplete/components/search-dialog/components/search-results/components/loading-skeleton/loading-skeleton.component.tsx
@@ -1,7 +1,7 @@
 const LoadingSkeleton = () => (
-  <ul className="animate-pulse overflow-y-auto flex flex-col gap-1">
+  <ul className="animate-pulse overflow-y-auto flex flex-col gap-1" data-testid="loading-skeleton">
     {Array.from({ length: 10 }).map((_, index) => (
-      <li key={index} className="w-full min-h-7 bg-slate-700 rounded" />
+      <li key={index} className="w-full min-h-7 bg-slate-700 rounded" data-testid={`loading-skeleton__item-${index}`} />
     ))}
   </ul>
 )
---
+++ src/features/github-autocomplete/components/search-dialog/components/search-results/components/loading-skeleton/loading-skeleton.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react"
+import LoadingSkeleton from "./loading-skeleton.component"
+import { describe, it, expect } from "vitest"
+
+describe("LoadingSkeleton", () => {
+  it("renders the correct number of skeleton items", () => {
+    render(<LoadingSkeleton />)
+    const skeletonItems = screen.getByTestId("loading-skeleton").children
+    expect(skeletonItems).toHaveLength(10)
+  })
+
+  it("has the correct class names for styling", () => {
+    render(<LoadingSkeleton />)
+    const skeletonItem = screen.getByTestId("loading-skeleton__item-0")
+    expect(skeletonItem).toHaveClass("w-full min-h-7 bg-slate-700 rounded")
+  })
+})
